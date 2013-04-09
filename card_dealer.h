37e8c118a227d1778b2f7b87ca818547475c2c05
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-04-09 09:24:56 -0500

    Relocated Dealer Class (No Sharing with Deck)

--- card_dealer.h
+++ card_dealer.h
@@ -1,11 +1,11 @@
 /******************************************************************************
-    card_collection.h
+    card_dealer.h
 
   Copyright (c) 2013 Jeffrey Carpenter
 
 ******************************************************************************/
-#ifndef CARD_COLLECTION_HEADERS
-#define CARD_COLLECTION_HEADERS
+#ifndef CARD_DEALER_HEADERS
+#define CARD_DEALER_HEADERS
 
 #include <array>
 #include <fstream>
@@ -18,25 +18,10 @@
 #include "SDL/SDL.h"
 
 #include "cfg.h"
+
 #include "card.h"
 
-//#define DEBUG_DECK
-//#define DEBUG_DEALER
-
-class Deck
-{
-  public:
-    Deck ( void );
-    ~Deck ( void );
-
-    bool Load ( std::string filename = CARDS_DB );
-    bool List ( void ); // debug
-
-    std::vector<Card> cards;
-
-  private:
-    // ...
-};
+//#define DEBUG_CARD_DEALER
 
 class Dealer
 {
@@ -56,4 +41,4 @@ class Dealer
     // ...
 };
 
-#endif // CARD_COLLECTION_HEADERS defined
+#endif // CARD_DEALER_HEADERS defined
