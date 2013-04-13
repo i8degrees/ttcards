4fcf65acbcaf36a4d645cbdbd18cd8f7f33b5468
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-04-13 11:42:54 -0500

    Converted *.bmp to PNG (Alpha Channels Handled Much Better in IMG_Load)

--- cfg.cpp
+++ cfg.cpp
@@ -38,26 +38,26 @@ const int BOARD_ORIGIN_Y = 18;
 
 const int BOARD_GRID_WIDTH = 3;
 const int BOARD_GRID_HEIGHT = 3;
-const char BOARD_BACKGROUND[255] = "./data/images/board.bmp";
+const char BOARD_BACKGROUND[255] = "./data/images/board.png";
 
 const char CARDS_DB[255] = "./data/cards.txt";
 const char CARD_FONTFACE[255] = "./data/fonts/EnvyCodeRb.ttf";
 const char SCORE_FONTFACE[255] = "./data/fonts/EnvyCodeRb.ttf";
 
-const char ELEMENT_NONE[255] = "./data/images/elements/none_16.bmp";
-const char ELEMENT_EARTH[255] = "./data/images/elements/earth_16.bmp";
-const char ELEMENT_FIRE[255] = "./data/images/elements/fire_16.bmp";
-const char ELEMENT_HOLY[255] = "./data/images/elements/holy_16.bmp";
-const char ELEMENT_ICE[255] = "./data/images/elements/ice_16.bmp";
-const char ELEMENT_POISON[255] = "./data/images/elements/poison_16.bmp";
-const char ELEMENT_THUNDER[255] = "./data/images/elements/thunder_16.bmp";
-const char ELEMENT_WATER[255] = "./data/images/elements/water_16.bmp";
-const char ELEMENT_WIND[255] = "./data/images/elements/wind_16.bmp";
+const char ELEMENT_NONE[255] = "./data/images/elements/none_16.png";
+const char ELEMENT_EARTH[255] = "./data/images/elements/earth_16.png";
+const char ELEMENT_FIRE[255] = "./data/images/elements/fire_16.png";
+const char ELEMENT_HOLY[255] = "./data/images/elements/holy_16.png";
+const char ELEMENT_ICE[255] = "./data/images/elements/ice_16.png";
+const char ELEMENT_POISON[255] = "./data/images/elements/poison_16.png";
+const char ELEMENT_THUNDER[255] = "./data/images/elements/thunder_16.png";
+const char ELEMENT_WATER[255] = "./data/images/elements/water_16.png";
+const char ELEMENT_WIND[255] = "./data/images/elements/wind_16.png";
 
 const char CARD_FACES[255] = "./data/images/faces/";
-const char HIDDEN_CARD[255] = "./data/images/faces/noface.bmp";
-const char PLAYER1_CARD[255] = "./data/images/player1.bmp";
-const char PLAYER2_CARD[255] = "./data/images/player2.bmp";
+const char HIDDEN_CARD[255] = "./data/images/faces/noface.png";
+const char PLAYER1_CARD[255] = "./data/images/player1.png";
+const char PLAYER2_CARD[255] = "./data/images/player2.png";
 const char NOPLAYER_CARD[255] = "./data/images/noplayer.png";
 
 const char LEFT_CURSOR[255] = "./data/images/left_cursor.png";
