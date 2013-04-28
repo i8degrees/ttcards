f886bfbd1041f64b2435f4319e173a6c19e544cd
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-04-27 22:19:25 -0500

    Additional Sanity Checking; Don't Bother to Draw if ID == 0

--- src/card_view.cpp
+++ src/card_view.cpp
@@ -68,86 +68,88 @@ bool CardView::EraseCard ( Gfx *engine, unsigned int x, unsigned int y )
 
 bool CardView::DrawCard ( Gfx *engine, Card &card, unsigned int x, unsigned int y )
 {
-  switch ( card.player_id ) // player1 (0), player2 (1)
+  if ( card.id != 0 )
   {
-    case 0:
-      card_background->SetSheetID ( PLAYER1_BACKGROUND_ID );
-      break;
-    case 1:
-      card_background->SetSheetID ( PLAYER2_BACKGROUND_ID );
-      break;
-    default:
-      card_background->SetSheetID ( NOPLAYER_BACKGROUND_ID );
-      break;
-  }
+    switch ( card.player_id ) // player1 (0), player2 (1)
+    {
+      case 0:
+        card_background->SetSheetID ( PLAYER1_BACKGROUND_ID );
+        break;
+      case 1:
+        card_background->SetSheetID ( PLAYER2_BACKGROUND_ID );
+        break;
+      default:
+        card_background->SetSheetID ( NOPLAYER_BACKGROUND_ID );
+        break;
+    }
 
-  card_background->SetX ( x + 0 );
-  card_background->SetY ( y + 0 );
-  if ( card_background->Draw ( engine ) == false )
-    return false;
+    card_background->SetX ( x + 0 );
+    card_background->SetY ( y + 0 );
+    if ( card_background->Draw ( engine ) == false )
+      return false;
 
-  card_face->SetSheetID ( card.id );
-  card_face->SetX ( x );
-  card_face->SetY ( y );
+    card_face->SetSheetID ( card.id );
+    card_face->SetX ( x );
+    card_face->SetY ( y );
 
-  if ( card_face->Draw ( engine ) == false )
-    return false;
+    if ( card_face->Draw ( engine ) == false )
+      return false;
 
-  switch ( card.element )
-  {
-    case 0: // N/A
-      card_element->SetSheetID ( ELEMENT_NONE );
-      break;
-    case 1: // EARTH
-      card_element->SetSheetID ( ELEMENT_EARTH );
-      break;
-    case 2: // FIRE
-      card_element->SetSheetID ( ELEMENT_FIRE );
-      break;
-    case 3: // HOLY
-      card_element->SetSheetID ( ELEMENT_HOLY );
-      break;
-    case 4: // ICE
-      card_element->SetSheetID ( ELEMENT_ICE );
-      break;
-    case 5: // POISON
-      card_element->SetSheetID ( ELEMENT_POISON );
-      break;
-    case 6: // THUNDER
-      card_element->SetSheetID ( ELEMENT_THUNDER );
-      break;
-    case 7: // WATER
-      card_element->SetSheetID ( ELEMENT_WATER );
-      break;
-    case 8: // WIND
-      card_element->SetSheetID ( ELEMENT_WIND );
-      break;
-    default:
-      card_element->SetSheetID ( ELEMENT_NONE );
-      break;
-  }
+    switch ( card.element )
+    {
+      case 0: // N/A
+        card_element->SetSheetID ( ELEMENT_NONE );
+        break;
+      case 1: // EARTH
+        card_element->SetSheetID ( ELEMENT_EARTH );
+        break;
+      case 2: // FIRE
+        card_element->SetSheetID ( ELEMENT_FIRE );
+        break;
+      case 3: // HOLY
+        card_element->SetSheetID ( ELEMENT_HOLY );
+        break;
+      case 4: // ICE
+        card_element->SetSheetID ( ELEMENT_ICE );
+        break;
+      case 5: // POISON
+        card_element->SetSheetID ( ELEMENT_POISON );
+        break;
+      case 6: // THUNDER
+        card_element->SetSheetID ( ELEMENT_THUNDER );
+        break;
+      case 7: // WATER
+        card_element->SetSheetID ( ELEMENT_WATER );
+        break;
+      case 8: // WIND
+        card_element->SetSheetID ( ELEMENT_WIND );
+        break;
+    }
 
-  card_element->SetX ( x + 46 );
-  card_element->SetY ( y + 4 );
-  if ( card_element->Draw ( engine ) == false )
-    return false;
+    card_element->SetX ( x + 46 );
+    card_element->SetY ( y + 4 );
+    if ( card_element->Draw ( engine ) == false )
+      return false;
 
-  #ifdef DEBUG_CARD_VIEW
-    this->text_buffer.SetTextBuffer ( std::to_string ( card.id ) );
-    this->text_buffer.DrawText ( engine, x+40, y+0 );
-  #endif
+    #ifdef DEBUG_CARD_VIEW
+      this->text_buffer.SetTextBuffer ( std::to_string ( card.id ) );
+      this->text_buffer.DrawText ( engine, x+40, y+0 );
+    #endif
 
-  this->text_buffer.SetTextBuffer ( std::to_string ( card.rank[0] ) );
-  this->text_buffer.DrawText ( engine, x+8, y+0 );
+    this->text_buffer.SetTextBuffer ( std::to_string ( card.rank[0] ) );
+    this->text_buffer.DrawText ( engine, x+8, y+0 );
 
-  this->text_buffer.SetTextBuffer ( std::to_string ( card.rank[1] ) );
-  this->text_buffer.DrawText ( engine, x+12, y+8 );
+    this->text_buffer.SetTextBuffer ( std::to_string ( card.rank[1] ) );
+    this->text_buffer.DrawText ( engine, x+12, y+8 );
 
-  this->text_buffer.SetTextBuffer ( std::to_string ( card.rank[2] ) );
-  this->text_buffer.DrawText ( engine, x+8, y+16 );
+    this->text_buffer.SetTextBuffer ( std::to_string ( card.rank[2] ) );
+    this->text_buffer.DrawText ( engine, x+8, y+16 );
 
-  this->text_buffer.SetTextBuffer ( std::to_string ( card.rank[3] ) );
-  this->text_buffer.DrawText ( engine, x+4, y+8 );
+    this->text_buffer.SetTextBuffer ( std::to_string ( card.rank[3] ) );
+    this->text_buffer.DrawText ( engine, x+4, y+8 );
 
-  return true;
+    return true;
+  } // if card.id != 0
+
+  return false;
 }
