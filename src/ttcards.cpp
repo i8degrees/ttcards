b85324c417a261c91c8d633a7a845eb9eba98b6f
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-05-24 04:32:14 -0500

    Memory leak fix due to SDLInput bug fix; allows changing state properly

--- src/ttcards.cpp
+++ src/ttcards.cpp
@@ -533,7 +533,7 @@ void TTcards::onKeyDown ( SDLKey key, SDLMod mod )
     case SDLK_d: if ( mod == KMOD_LMETA ) this->removePlayerCard(); break;
 
     case SDLK_i: debugBox(); break;
-    case SDLK_r: this->engine->PushState ( std::unique_ptr<CardsMenu>( new CardsMenu ( this->engine ) ) ); break;
+    case SDLK_r: this->engine->PopStateThenChangeState ( std::unique_ptr<CardsMenu>( new CardsMenu ( this->engine ) ) ); break;
 
     case SDLK_LEFT: this->moveCursorLeft(); break;
     case SDLK_RIGHT: this->moveCursorRight(); break;
