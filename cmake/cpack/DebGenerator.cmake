e30272c4bc8f9361f81fbf6473b3351a47d11e8e
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-07-28 02:29:39 -0500

    Add package installation for OSX (dmg), Linux (deb) & generic (zip)

---
+++ cmake/cpack/DebGenerator.cmake
@@ -0,0 +1,9 @@
+#     CPack Linux "DEB" generator
+
+#set ( CPACK_DEBIAN_PACKAGE_DEPENDS "libboost-dev libmagic-dev libsndfile1-dev libopenal-dev libsdl1.2-dev libsdl1.2debian libsdl-image1.2-dev libsdl-image1.2 libsdl-ttf2.0-dev libsdl-ttf2.0-0" )
+set ( CPACK_DEBIAN_PACKAGE_PRIORITY "optional" )
+set ( CPACK_DEBIAN_PACKAGE_SECTION "games" )
+set ( CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64" )
+
+#set ( CMAKE_INSTALL_PREFIX "/usr/local" )
+set ( CPACK_PACKAGING_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}" )
---
+++ cmake/cpack/OSXGenerator.cmake
@@ -0,0 +1,4 @@
+#     CPack OSX "DragNDrop" generator
+
+#set ( CMAKE_INSTALL_PREFIX "/Applications" )
+set ( CPACK_PACKAGING_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}" )
