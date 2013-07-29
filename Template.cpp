07394e6a07c3c6ddb7b13395fdfd29ef14c5f835
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-07-29 07:31:11 -0500

    Introduce helper source & header file templates for internal dev

---
+++ Template.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+    C++ Template Class; don't forget to add me to CMakeLists.txt
+
+  Copyright (c) 2013 Jeffrey Carpenter
+  All rights reserved.
+
+******************************************************************************/
+#include "Template.hpp"
+
+Template::Template ( void )
+{
+  // Initialize instance variables
+}
+
+Template::~Template ( void )
+{
+  // Clean up instance variables
+}
---
+++ Template.hpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+    C++ Template Header; don't forget to add me to CMakeLists.txt
+
+  Copyright (c) 2013 Jeffrey Carpenter
+  All rights reserved.
+
+******************************************************************************/
+#ifndef GAMEAPP_TEMPLATE_HPP
+#define GAMEAPP_TEMPLATE_HPP
+
+#include <iostream>
+#include <string>
+
+#include "cfg.h"
+
+class Template
+{
+  public:
+    Template ( void );
+    ~Template ( void );
+
+  private:
+    // ...
+};
+
+#endif // include guard defined
