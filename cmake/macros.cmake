1eb383f0fb679cb4a1241b1e905fa69f6dbc0e7a
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-08-31 05:05:50 -0500

    Introduce quiet (no output) on uninstall & rpath macros

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -1,16 +1,27 @@
 # Helper macros for CMake build scripts
 #
 
+# Be quiet about what we are doing if verbose makefiles are toggled off
+if ( NOT CMAKE_VERBOSE_MAKEFILE )
+  set ( COMMENT_TEXT )
+endif ( NOT CMAKE_VERBOSE_MAKEFILE )
+
 # Add runtime search path to our application bundle so that we can find its
 # dependencies at launch.
 macro ( add_rpath rpath binary_path )
 
   foreach ( path ${rpath} )
+
+    if ( CMAKE_VERBOSE_MAKEFILE )
+      set ( COMMENT_TEXT "\nAdding runtime search path: \n\n\t${path}\n\nto ${binary_path}\n\n" )
+    endif ( CMAKE_VERBOSE_MAKEFILE )
+
     add_custom_command  ( TARGET ${PROJECT_NAME}
                           COMMAND ${CMAKE_INSTALL_NAME_TOOL}
                           -add_rpath "${path}" "${binary_path}"
-                          COMMENT "\nAdding runtime search path: \n\n\t${path}\n\nto ${binary_path}\n\n"
+                          COMMENT ${COMMENT_TEXT}
                         )
+
   endforeach ( path ${rpath} )
 
 endmacro ( add_rpath rpath binary_path )
