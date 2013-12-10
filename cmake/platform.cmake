8727ad5732b433227f7b9089620d7281d07e6979
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-12-10 01:51:53 -0600

    CMakeLists: Clean up various build options
        CMakeLists.txt:
    * Remove TTCARDS_DATA_DIR cmake variable; we now are setting this at
    run-time.
    
    * Introduce EXECUTABLE_FLAGS cmake variable; used for platform
    dependent control of binary output type.
    
        Windows platform:
    * Implement proper installation prefix support (mostly finished); the
    binary output dir is now build/Debug (alike nomlib). Resources
    dir is copied to this path.
    
    * Install third-party library dependencies from third-party --
    including MSVC++ redistributable DLLs -- into executable output dir;
    this allows us to freely copy the folder to other systems!
    
    * Set EXECUTABLE_FLAG to WIN32 when not a debug build type; otherwise
    we build as a Console app, for ease of debugging.
    
        cmake/templates/version.hpp.in:
    * Remove OSXAPP from project scope; we now only use this variable in
    our cmake build scripts -- not in our compiled source.

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -1,33 +1,26 @@
 # Platform detection script for CMake
 
 if ( CMAKE_SYSTEM_NAME STREQUAL "Darwin" )
-
   set ( PLATFORM_OSX true )
 
   option ( OSXAPP "Enable building OS X Application Bundle" on )
   option ( UNIVERSAL "Enable building OSX Universal Application" off )
 
-  message ( STATUS "Platform: Darwin (OS X)" )
-
+  message ( STATUS "Platform: Darwin (Mac OS X)" )
 elseif ( CMAKE_SYSTEM_NAME STREQUAL "Linux" )
-
   set ( PLATFORM_LINUX true )
+
   message ( STATUS "Platform: Linux" )
-
 elseif ( CMAKE_SYSTEM_NAME STREQUAL "Windows" )
-
   set ( PLATFORM_WINDOWS true )
 
   option ( ARCH_32 "Compile ${PROJECT_NAME} as a 32-bit library" off )
   option ( ARCH_64 "Compile ${PROJECT_NAME} as a 64-bit library" on )
 
   message ( STATUS "Platform: Windows" )
-
 else () # Not Linux nor OSX
-
   set ( PLATFORM_UNKNOWN true )
   message ( STATUS "Platform: Unknown" )
-
 endif ( CMAKE_SYSTEM_NAME STREQUAL "Darwin" )
 
 # TODO; we might be able to put these platform checks shown below in the
