f5a70231ac8eb46c7e4ac6f790bcc4c476b7ae8c
Jeffrey Carpenter <jeffrey.carp@gmail.com>
2013-12-11 22:25:12 -0600

    Move platform specific cfg to platform.cmake

--- cmake/platform.cmake
+++ cmake/platform.cmake
@@ -6,9 +6,19 @@ if ( CMAKE_SYSTEM_NAME STREQUAL "Darwin" )
   option ( OSXAPP "Enable building OS X Application Bundle" on )
   option ( UNIVERSAL "Enable building OSX Universal Application" off )
 
+  # libc++ requires OSX v10.7+
+  set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++" )
+
+  if ( CMAKE_GENERATOR STREQUAL "Xcode" )
+    set ( CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++11" )
+    set ( CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++" )
+  endif ( CMAKE_GENERATOR STREQUAL "Xcode" )
+
   message ( STATUS "Platform: Darwin (Mac OS X)" )
-elseif ( CMAKE_SYSTEM_NAME STREQUAL "Linux" )
+elseif ( CMAKE_SYSTEM_NAME STREQUAL "Linux" ) # Tested on Ubuntu v12.04-LTS
   set ( PLATFORM_LINUX true )
+
+  set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x" )
 
   message ( STATUS "Platform: Linux" )
 elseif ( CMAKE_SYSTEM_NAME STREQUAL "Windows" )
@@ -18,10 +28,15 @@ elseif ( CMAKE_SYSTEM_NAME STREQUAL "Windows" )
   option ( ARCH_64 "Compile ${PROJECT_NAME} as a 64-bit library" on )
 
   message ( STATUS "Platform: Windows" )
-else () # Not Linux nor OSX
+else () # Not OSX, Linux or Windows OS
   set ( PLATFORM_UNKNOWN true )
+
+  # Use whatever CMake gathers for us and pray for the best!
+
   message ( STATUS "Platform: Unknown" )
 endif ( CMAKE_SYSTEM_NAME STREQUAL "Darwin" )
+
+message ( STATUS "Generating build files for: ${CMAKE_GENERATOR}" )
 
 # TODO; we might be able to put these platform checks shown below in the
 # CMAKE_SYSTEM_NAME checks above.
@@ -30,6 +45,7 @@ if ( UNIVERSAL )
   set ( PLATFORM_ARCH "x86; x64" ) # Reserved for future use
 
 else ( NOT UNIVERSAL )
+  # TODO: We should probably consider bumping the architecture down to i386 $(ARCHS_STANDARD_32_BIT)
   set ( CMAKE_OSX_ARCHITECTURES x86_64 )
   set ( PLATFORM_ARCH "x64" ) # Reserved for future use
 
