2e52585b69c74eaba29c8bcfa9e4149121ec5b48
Jeffrey Carpenter <i8degrees@gmail.com>
2014-11-03 14:00:49 -0600

    bin/fswatch.sh: Fix while loop && add script output
    This uses a proper while loop implementation and outputs the file
    copy results upon a fswatch event. The use of a proper while loop
    drops CPU usage of fswatch from ~99% to ~0%.

--- bin/fswatch.sh
+++ bin/fswatch.sh
@@ -3,11 +3,25 @@
 # https://github.com/emcrisostomo/fswatch
 
 # File or directory path
-WATCH_PATH=${HOME}/Projects/ttcards.git/Resources/config.json
 CP_BIN=/bin/cp        # BSD
 # CP_BIN=$(which cp)  # GNU
 
-# FIXME: There is currently no output from this script =(
-fswatch -o ${WATCH_PATH} | while true; do
-  ${CP_BIN} ${WATCH_PATH} ${HOME}/Documents/ttcards/config.json
+# source path to watch for file mods
+WATCH_PATH=${HOME}/Projects/ttcards.git/Resources/config.json
+
+# destination path to copy modified file to
+DEST_PATH=${HOME}/Documents/ttcards/config.json
+
+echo "${0}: Watching ${WATCH_PATH} for file modifications"
+
+WATCHING=true
+fswatch -0 "${WATCH_PATH}" | while [ $WATCHING == true ]
+do
+  read -d "" EVENT
+  # echo "${event}"
+
+  if [[ ${EVENT} != "" ]]; then
+    ${CP_BIN} -v ${WATCH_PATH} ${DEST_PATH}
+  fi
+
 done
