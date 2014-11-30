a73f9ae5250f1dccd4817f9c84bdeed6db31b192
Jeffrey Carpenter <i8degrees@gmail.com>
2014-11-30 11:21:09 -0600

    Use nomlib's modularized find module
    * nomlib's new find module includes automatic dependency
    management for its external libraries, freeing us from the burden of
    manually setting it up -- w00t!
    
    * platform.cmake: Clean up how the OSX SDK is set; passing
    -D CMAKE_OSX_DEPLOYMENT_TARGET=<ver> takes care of everything that we
    had to do manually before, with much cleaner syntax.

--- src/config.hpp
+++ src/config.hpp
@@ -31,7 +31,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <string>
 
-#include "nomlib/config.hpp"
+#include <nomlib/config.hpp>
 #include <nomlib/math.hpp>
 
 #include <cassert>
