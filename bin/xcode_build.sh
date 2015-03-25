300b0642f28a2c492ff802c3b0275f0509af3bc2
Jeffrey Carpenter <i8degrees@gmail.com>
2015-03-25 08:04:47 -0500

    Update xcode build scripts to use nomlib's improved ver
    This gives us a few new optional features: a) passing of the build
    configuration type, defaulting to Debug; b) passing of the num of
    threads xcodebuild is allowed to use; c) bit of additoinal status
    output of what the shell script is doing.
    
    * bin/xcode_install.sh: Add failure output when this script opts
    out of trying to do anything. Additionally, the exit code for
    this script under said failure condition has been changed from
    always returning 1 to now match the exit code of the failed command,
    for sake of clarity.

--- bin/xcode_build.sh
+++ bin/xcode_build.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
 
+# NOTE: This script is intended to be ran from the project's current build
+# directory.
+
+XCODEBUILD_BIN=$(which xcodebuild)
+BUILD_TYPE_ARG=$1
+
+# Default
+BUILD_TYPE="Debug"
+
+if [[ !( -z "${BUILD_TYPE_ARG}") ]]; then
+  BUILD_TYPE=$1
+fi
+
+if [[ !( -z "${NUM_THREADS}") ]]; then
+  NUM_THREADS_ARG="-jobs ${NUM_THREADS}"
+fi
+# echo "NUM_THREADS_ARG: ${NUM_THREADS_ARG}"
+
 ../bin/check_json.sh
 
 if [[ $? -eq 0 ]]; then
-  xcodebuild -configuration Debug -target ALL_BUILD
+  echo "Building ${BUILD_TYPE} project... [target: build]"
+  ${XCODEBUILD_BIN} ${NUM_THREADS_ARG} -configuration ${BUILD_TYPE} \
+  -target ALL_BUILD
 else
   echo "${0} ERROR: JSON validation failure has prevented building the project."
   exit 1
+fi
 fi
