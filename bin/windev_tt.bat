5168eb54dc5aa6c9ed20b61a85f721c976ad70ef
Jeffrey Carpenter <i8degrees@gmail.com>
2015-05-24 17:33:31 -0500

    Add automated Windows build script using msbuild
    This is the local configuration used on windev.local.

---
+++ bin/windev_tt.bat
@@ -0,0 +1,22 @@
+@echo off
+
+REM Automated Windows build script for TTcards -- windev.local
+
+REM NOTE: This script is intended to be ran from the project's root
+REM directory, i.e.: %HOME%/Projects/ttcards.git
+
+REM ...Environment setup...
+set NOMLIB_DEPS_PREFIX=%HOME%/Projects/nomlib.git/third-party/windows
+
+REM virgo.local source repository
+unison ttcards
+
+IF NOT EXIST build mkdir build
+REM pushd build
+cd build
+
+if errorlevel 0 call ..\bin\msbuild_gen.bat Debug
+if errorlevel 0 call ..\bin\msbuild_build.bat Debug
+if errorlevel 0 call ..\bin\msbuild_install.bat Debug
+
+REM popd
